a77334131253065418dba10e38fc2b5c08557f19
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-06-04 23:31:59 +0300

    Update
    Updated for character-list-component.

--- rick-and-morty-app/src/app/components/character-list/character-list.component.ts
+++ rick-and-morty-app/src/app/components/character-list/character-list.component.ts
@@ -1,21 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharacterService } from '../../services/character.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-character-list',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  styleUrls: ['./character-list.component.scss'],
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit {
   characters: any[] = [];
+  currentPage: number = 1;
+  nameFilter: string = '';
+  speciesFilter: string = '';
+  statusFilter: string = '';
+  genderFilter: string = '';
 
-  constructor(private characterService: CharacterService) {
-    this.characterService.getCharacters().subscribe(response => {
-      this.characters = response.results;
-    });
+  constructor(private characterService: CharacterService) {}
+
+  ngOnInit(): void {
+    this.getCharacters(this.currentPage);
+  }
+
+  getCharacters(page: number): void {
+    this.characterService.filterCharacters(this.nameFilter, this.speciesFilter, this.statusFilter, this.genderFilter)
+      .subscribe(response => {
+        this.characters = response.results;
+      });
+  }
+
+  filterCharacters(): void {
+    this.currentPage = 1;
+    this.getCharacters(this.currentPage);
+  }
+
+  clearFilters(): void {
+    this.nameFilter = '';
+    this.speciesFilter = '';
+    this.statusFilter = '';
+    this.genderFilter = '';
+    this.getCharacters(this.currentPage);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.getCharacters(this.currentPage);
+    }
+  }
+
+  nextPage(): void {
+    this.currentPage++;
+    this.getCharacters(this.currentPage);
   }
 }
--- rick-and-morty-app/src/app/services/character.service.ts
+++ rick-and-morty-app/src/app/services/character.service.ts
@@ -3,22 +3,28 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CharacterService {
   private apiUrl = 'https://rickandmortyapi.com/api';
 
   constructor(private http: HttpClient) {}
 
-  getCharacters(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/character`);
+  getCharacters(page: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/character/?page=${page}`);
   }
 
   getCharacter(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/character/${id}`);
   }
 
-  searchCharacters(query: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/character/?name=${query}`);
+  filterCharacters(name: string, species: string, status: string, gender: string): Observable<any> {
+    let filterParams = '';
+    if (name) filterParams += `&name=${name}`;
+    if (species) filterParams += `&species=${species}`;
+    if (status) filterParams += `&status=${status}`;
+    if (gender) filterParams += `&gender=${gender}`;
+
+    return this.http.get<any>(`${this.apiUrl}/character/?${filterParams}`);
   }
 }
